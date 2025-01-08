1312e4a87140886aa530be5f6027ce2eb5926c00
FTC18035 <ftc18035@gmail.com>
2025-01-08 16:44:59 -0500

    Started migrating to the library. Kinda hit a wall, trying to get info from the discord

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+
+import com.pedropathing.localization.Localizers;
+import com.pedropathing.follower.FollowerConstants;
+
+public class FConstants {
+    static {
+        FollowerConstants.localizers = Localizers.TWO_WHEEL;
+
+        FollowerConstants.mass = 16.60148;
+
+    }
+}
